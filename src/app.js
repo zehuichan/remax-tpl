1fd0d444af8b6e047c8d05557d822eaf2789517d
chenzehui <jasonchenzehui@gmail.com>
2021-02-08 11:37:20 +0800

    d

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import * as React from 'react'
+import React from 'react'
+import {useAppEvent} from 'remax/macro'
 import dva from 'remax-dva'
 
 // global css
@@ -7,6 +8,18 @@ import '@/assets/less/index.less'
 const app = dva()
 
 const App = app.start(({ children }) => {
+
+  useAppEvent('onLaunch', () => {
+    console.log('app onLaunch')
+  })
+
+  useAppEvent('onShow', () => {
+    console.log('app onShow')
+  })
+
+  useAppEvent('onHide', () => {
+    console.log('app onHide')
+  })
 
   return children
 })
--- src/pages/tabs/components/index.js
+++ src/pages/tabs/components/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import './index.less'
+import {usePageEvent} from 'remax/macro'
 import useUserInfo from '@/hooks/useUserInfo'
 import {View} from 'remax/ali'
 import RAuth from '@/components/RAuth'
@@ -11,6 +12,14 @@ import Badge from 'mini-ali-ui/es/badge'
 
 const Components = () => {
   const [user, login] = useUserInfo()
+  usePageEvent('onShow', () => {
+    console.log('page onShow')
+  })
+
+  usePageEvent('onLoad', () => {
+    console.log('page onLoad')
+  })
+
   return (
     <View className="components">
       <Container
